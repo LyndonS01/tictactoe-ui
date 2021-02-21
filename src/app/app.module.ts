8b84f9e301dd53d1db2af1e4276229e061da92d2
Lyndon Santos <lyndonjohn.santos@cognizant.com>
2021-02-20 20:05:06 -0800

    adds cellComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CellsComponent } from './cells/cells.component';
+import { CellsComponent } from './components/cells/cells.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CellsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CellsComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/cells/cells.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cells',
+  templateUrl: './cells.component.html',
+  styleUrls: ['./cells.component.css'],
+})
+export class CellsComponent implements OnInit {
+  opponent: string = '';
+  opponentSelected: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  humanButtonClicked() {
+    this.opponent = 'Human';
+    this.opponentSelected = true;
+  }
+}
