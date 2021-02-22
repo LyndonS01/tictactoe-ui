ee6cc98ab216a7f363d63d13dbf5965d79c9d89a
Lyndon Santos <lyndonjohn.santos@cognizant.com>
2021-02-22 08:49:11 -0800

    refines home page formatting

--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -1,4 +1,4 @@
-import { ICurrentBoard } from './currentboard';
+// import { ICurrentBoard } from './currentboard';
 
 export interface IGame {
   gameId: number;
@@ -8,4 +8,19 @@ export interface IGame {
   winner: string;
   winningLine: number;
   currentBoard: ICurrentBoard;
+}
+
+interface ICurrentBoard {
+  boardId: number;
+  p1Symbol: string;
+  p2Symbol: string;
+  pos0: string;
+  pos1: string;
+  pos2: string;
+  pos3: string;
+  pos4: string;
+  pos5: string;
+  pos6: string;
+  pos7: string;
+  pos8: string;
 }
