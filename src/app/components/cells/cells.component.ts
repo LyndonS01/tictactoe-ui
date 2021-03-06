05fd6a88fe29bfa370042c91c2219299e164c0d9
LyndonS01 <santos.lyndon@gmail.com>
2021-03-06 09:24:17 -0800

    fixes messaging in humanButtonClickedmethod

--- src/app/components/cells/cells.component.ts
+++ src/app/components/cells/cells.component.ts
@@ -26,6 +26,7 @@ export class CellsComponent implements OnInit {
   filledPositions = 0;
   humanOpponent = false;
   board = new SymbolsModel();
+  boardLocked = false;
 
   constructor(
     private gameService: GameService,
@@ -47,25 +48,32 @@ export class CellsComponent implements OnInit {
     };
 
     // send the appropriate prompt message
-    let symbol = '';
     if (this.game !== undefined) {
-      if (this.game.nextMove === this.game.player1) {
-        symbol = this.game.currentBoard.p1Symbol;
-      } else {
-        symbol = this.game.currentBoard.p2Symbol;
+      // is it my turn to move?
+      if (this.game.nextMove === this.username) {
+        // pick up my symbol
+        let symbol;
+        if (this.game.currentBoard.p1Symbol === this.username) {
+          symbol = this.game.currentBoard.p1Symbol;
+        } else {
+          symbol = this.game.currentBoard.p2Symbol;
+        }
+        this.messagesService.add(
+          `Your move, ${this.game.nextMove} (${symbol})`
+        );
       }
-      this.messagesService.add(`Your move, ${this.game.nextMove} (${symbol})`);
-    } else {
-      // current player is initiating the game
-      this.messagesService.add('Waiting for opponent to join or move...');
     }
 
+    this.messagesService.add(`Make the first move, ${this.username}`);
+
+    this.boardLocked = true;
     this.gameService.newGame(newGameParams).subscribe({
       next: (game) => {
         (this.game = game),
           (this.gameId = game.gameId),
           (this.opponent = game.player2),
-          this.copyBoard(game.currentBoard);
+          this.copyBoard(game.currentBoard),
+          this.toggleBoardLock(game);
       },
       // error: (err) => (this.errorMessage = err),
     });
@@ -81,11 +89,13 @@ export class CellsComponent implements OnInit {
 
     this.messagesService.add('Your opponent is the game server');
     this.messagesService.add(`Your move, ${this.username} (O)`);
+    this.boardLocked = true;
     this.gameService.newGame(newGameParams).subscribe({
       next: (game) => {
         (this.game = game),
           (this.gameId = game.gameId),
-          this.copyBoard(game.currentBoard);
+          this.copyBoard(game.currentBoard),
+          this.toggleBoardLock(game);
       },
       // error: (err) => (this.errorMessage = err),
     });
@@ -99,7 +109,7 @@ export class CellsComponent implements OnInit {
     this.winner = '';
     this.messagesService.clear();
     this.humanOpponent = false;
-    // console.log(`Message length: ${this.messagesService.messages.length}`);
+    this.boardLocked = false;
   }
 
   positionButtonClicked(position: number): void {
@@ -122,6 +132,7 @@ export class CellsComponent implements OnInit {
         position: this.position,
       };
 
+      this.boardLocked = true;
       this.gameService.sendMove(moveParams).subscribe({
         next: (game) => {
           (this.game = game),
@@ -129,7 +140,8 @@ export class CellsComponent implements OnInit {
             (this.winningLine = game.winningLine),
             (this.winner = game.winner),
             this.copyBoard(game.currentBoard),
-            this.checkWinOrDraw(game.currentBoard);
+            this.checkWinOrDraw(game.currentBoard),
+            this.toggleBoardLock(game);
         },
         // error: (err) => (this.errorMessage = err),
       });
@@ -204,5 +216,17 @@ export class CellsComponent implements OnInit {
     this.board.symbol[6] = currentBoard.pos6;
     this.board.symbol[7] = currentBoard.pos7;
     this.board.symbol[8] = currentBoard.pos8;
+  }
+
+  toggleBoardLock(game: IGame): void {
+    if (this.humanOpponent) {
+      if (this.username !== game.nextMove) {
+        this.boardLocked = true;
+      } else {
+        this.boardLocked = false;
+      }
+    } else {
+      this.boardLocked = false;
+    }
   }
 }
