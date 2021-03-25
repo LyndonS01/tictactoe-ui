48c710699a8f14acfdaa1eaefa0d8ff20834f5e6
Lyndon Santos <lyndonjohn.santos@cognizant.com>
2021-03-25 16:39:58 -0700

    updates configuration files to run docker containers by batch file

---
+++ runp1.bat
@@ -0,0 +1,1 @@
+docker run -d -it --network tictactoe_net --name tictactoe-p1-ui -p 8082:80 tictactoe-ui
--- runp2.bat
+++ runp2.bat
@@ -1,1 +1,1 @@
-docker run -d -it -p 8082:80 tictactoe-p2-ui
+docker run -d -it --network tictactoe_net --name tictactoe-p2-ui -p 8083:80 tictactoe-ui
