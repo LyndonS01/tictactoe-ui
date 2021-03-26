a6283b8ade55e03283d393980781f2afea33ffbe
Lyndon Santos <lyndonjohn.santos@cognizant.com>
2021-03-26 11:02:45 -0700

    deletes batch files

--- runp1.bat
+++
@@ -1,1 +0,0 @@
-docker run -d -it --network tictactoe_net --name tictactoe-p1-ui -p 8082:80 tictactoe-ui
--- runp2.bat
+++
@@ -1,1 +0,0 @@
-docker run -d -it --network tictactoe_net --name tictactoe-p2-ui -p 8083:80 tictactoe-ui
